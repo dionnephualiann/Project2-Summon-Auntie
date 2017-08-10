3dbc74cfbb3cd7af417e6554c83636ac8fa08293
Dionne Phua <li.ann86@gmail.com>
2017-08-10 18:34:56 +0800

    add console log to facebook callback auth

--- app.js
+++ app.js
@@ -172,6 +172,7 @@ app.delete('/api/markers', passportConfig.isAuthenticated, markerController.remo
  */
 app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'public_profile'] }));
 app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), (req, res) => {
+  console.log("test Login facebook")
   res.redirect('/');
 });
 
